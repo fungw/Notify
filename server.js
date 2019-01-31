08074d31fa808ff098b0b27541c74fd6c7f2109a
Wesley Fung <yutacan@ManHoo.local>
2019-01-31 15:23:13 +0000

    Add path package

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
@@ -8,14 +9,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
+
 app.post('/api/world', (req, res) => {
   console.log(req.body);
   res.send(
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
 });
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
@@ -25,4 +25,5 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
